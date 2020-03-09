6ca4b75d1f85ac34cba8e0ada4f86e415d28f4dd
Samantha Meason <sbmeason@gmail.com>
2020-03-09 21:42:18 +0000

    add notes for cli class

---
+++ CLIproject/lib/CLIproject.rb
@@ -0,0 +1,10 @@
+require "CLIproject/version"
+require "CLIproject/cli"
+
+# dependencies 
+require "pry"
+
+module CLIproject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ CLIproject/lib/CLIproject/cli.rb
@@ -0,0 +1,28 @@
+class CLIproject::CLI
+ 
+ #start should greet user, maybe give description
+ #get data from scraper/api file
+ #create new custom objects
+ #all inside start method
+ 
+ def start
+    puts "Hello there!"
+    puts "GETTING DATA FROM API...PLEASE WAIT"
+    puts "CREATING NEW OBJS"
+  end
+  
+ 
+ #deal with inputs (loop to keep asking to get new info)
+ #EX. while input does not equal exit do 
+ #display a list of something or give examples of what we expect as input
+ #get user input
+ #depending on what we get, do something
+ #condition to check input for good value
+ #else tell them try again
+ 
+ #exit command
+ #if input == "EXIT"
+ #kill program, say goodbye
+ 
+  
+end
